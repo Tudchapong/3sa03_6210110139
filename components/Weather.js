77677be9268dd02019633275d6872df2e01f0d06
Tudchapong <6210110139@psu.ac.th>
2021-07-15 11:18:41 +0700

    1.Forecast

--- components/Weather.js
+++ components/Weather.js
@@ -1,8 +1,23 @@
-import React  from 'react'
-import { Text } from 'react-native'
+import React, { useState }  from 'react'
+import { ImageBackground, Text } from 'react-native'
+import Forecast from './Forecast'
 
 export default function Weather(props){
+    const [forecastInfo, setForecastInfo] = useState({
+        main: '-',
+        descroption: '-',
+        temp: 0
+    })
     return(
-        <Text>{props.zipCode}</Text>
+        <ImageBackground source={require('rainy.jpg')} style={styles.backdrop}>
+            <Forecast {...forecastInfo}/>
+        </ImageBackground>
     )
 }
+
+const styles = StyleSheet.create({
+    backdrop:{
+        width: '100%',
+        height: '100%'
+    }
+})
